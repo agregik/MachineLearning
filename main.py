a16876520610924666de2ba892aabbd0837d784e
agregik <bokovenok11@mail.ru>
2023-03-24 22:59:13 +0500

    дукщгуен

--- main.py
+++
@@ -1,3 +0,0 @@
-for x in '0123456789ABCDEFG':
-    if (int('10' + str(x) + '0', 17) + int('f0' + str(x) + 'ff', 17)) % 13 == 0:
-        print((int('10' + str(x) + '0', 17) + int('F0' + str(x) + 'FF', 17)) // 13)
