3221bb48651752a14d8f982a62a7b62f9e7a5365
agregik <bokovenok11@mail.ru>
2023-09-09 01:38:41 +0500

    Машинное обучение Криса Элбона

--- machinelearning.py
+++ machinelearning.py
@@ -270,3 +270,12 @@ matrix_b = np.array([[1, 3, 1],
 
 # Сложить матрицы
 # print(np.add(matrix_b, matrix_a))
+# print(matrix_b + matrix_a)
+
+# Вычесть из одной матрицу другую
+# print(matrix_a - matrix_b)
+# print(np.subtract(matrix_a, matrix_b))
+
+# Умножить одну матрицу на другую
+# print(matrix_a * matrix_b)
+
