f6816d826531ce36f17e12eb06421b4b0b64f2bf
agregik <bokovenok11@mail.ru>
2023-09-23 03:48:14 +0500

    Machinelearning Криса Элбона (3 глава)

--- machinelaerning(3 part).py
+++ machinelaerning(3 part).py
@@ -10,7 +10,7 @@
 # Взглянуть на первые пять строк
 # print(dataframe(5))
 
-# 3.1 Создание фрейма данных
+# 3.1 Создание фрейма данных и 3.2 Описание данных
 
 import pandas as pd
 
@@ -18,10 +18,29 @@ import pandas as pd
 dataframe = pd.DataFrame()
 
 # Добавить столбцы
-dataframe['Имя'] = ['Никита Боков', 'Стивен Стивенсон']
+dataframe['Имя'] = ['Никита Боков', 'Рома Алиев']
 dataframe['Возраст'] = [18, 17]
 dataframe['Водитель'] = [True, False]
 
 # Создать строку и добавить её в конец фрейма данных
 dataframe.loc[len(dataframe.index)] = ['Анна Буланникова', 17, True]
-print(dataframe)
+dataframe.loc[len(dataframe.index)] = ['Гриша Боков', 13, False]
+# Взглянуть на фрейм данных
+# print(dataframe)
+
+# НАЧАЛО 3.2 ПУНКТА
+
+# Взглянуть на первую строку фрейма данных
+# print(dataframe.iloc[0])
+
+# Взглянуть на первые две строки
+# print(dataframe.head(2))
+
+# Взглянуть на размерность матрицы
+# print(dataframe.shape)
+
+# Взглянуть на статистику
+print(dataframe.describe())
+
+
+
